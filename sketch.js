f20e504879290b8dae2fe0b7858889f1c72c41ba
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-11-24 09:35:20 +0900

    파일 추가

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let emitter;
+
+let repeller;
+
+function setup() {
+  createCanvas(640, 240);
+  emitter = new Emitter(width / 2, 20);
+  repeller = new Repeller(width / 2, 200);
+}
+
+function draw() {
+  background(255);
+ 
+  
+  emitter.addParticle();
+
+  let gravity = createVector(0, 0.1);
+  emitter.applyForce(gravity);
+
+  emitter.applyRepeller(repeller);
+  emitter.run();
+
+  repeller.show();
+}
