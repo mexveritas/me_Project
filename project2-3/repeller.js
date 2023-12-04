4c4e71db62db6671c1b618e254a172c80f43674f
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-12-04 14:29:13 +0900

    project2-3추가

---
+++ project2-3/repeller.js
@@ -0,0 +1,25 @@
+class Repeller {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+
+    this.power = 10;
+  }
+  
+  show() {
+    stroke(0);
+    strokeWeight(2);
+    fill(0, 100, 250);
+    circle(this.position.x, this.position.y, 32);
+  }
+
+  repel(particle) {
+ 
+    let force = p5.Vector.sub(this.position, particle.position);
+    let distance = force.mag();
+    distance = constrain(distance, 5, 500);
+    let strength = (-1 * this.power) / (distance * distance);
+    force.setMag(strength);
+    return force;
+  }
+}
+
---
+++ project2-3/sketch.js
@@ -0,0 +1,39 @@
+let emitter;
+let repellers = [];
+let attractors = [];
+
+function setup() {
+  createCanvas(400, 400);
+  emitter = new Emitter(width / 2, height / 2);
+
+  for (let i = 0; i < 7; i++) {
+  repellers.push(new Repeller(random(width), random(height)));
+  }
+
+  for (let i = 0; i < 7; i++) {
+    attractors.push(new Attractor(random(width), random(height)));
+    }
+}
+
+
+function draw() {
+  background(220);
+ 
+  emitter.addParticle();
+
+  let gravity = createVector(0, 0.001);
+  emitter.applyForce(gravity);
+
+  for (let repeller of repellers) {
+    emitter.applyRepeller(repeller);
+    repeller.show();
+  }
+
+  for (let attractor of attractors) {
+    emitter.applyAttractor(attractor);
+    attractor.show();
+  }
+
+  emitter.run();
+
+}
