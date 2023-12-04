5ce2d13db44920d3b6dee2f9160210537a40db4c
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-12-04 13:23:35 +0900

    슬라이더 추가

--- project2-1/sketch.js
+++ project2-1/sketch.js
@@ -6,6 +6,18 @@ let attractor;
 
 function setup() {
   createCanvas(400, 400);
+
+  textSize(15);
+  noStroke();
+
+  // create sliders
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
+
   emitter = new Emitter(width / 2, height / 2);
   repeller = new Repeller(width / 2, 300);
   attractor = new Attractor(width /2 , height/2);
@@ -14,6 +26,14 @@ function setup() {
 
 function draw() {
   background(220);
+
+  const r = rSlider.value();
+  const g = gSlider.value();
+  const b = bSlider.value();
+  background(r, g, b);
+  text('red', rSlider.x * 2 + rSlider.width, 35);
+  text('green', gSlider.x * 2 + gSlider.width, 65);
+  text('blue', bSlider.x * 2 + bSlider.width, 95);
  
   emitter.addParticle();
 
@@ -32,4 +52,6 @@ function draw() {
 
   repeller.show();
   attractor.show();
+
+  
 }
