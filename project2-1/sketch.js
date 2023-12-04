db81bfce7201b29e7d2fc2d5771588579698e624
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-12-04 13:59:18 +0900

    오류 수정

--- project2-1/sketch.js
+++ project2-1/sketch.js
@@ -4,6 +4,8 @@ let repeller;
 
 let attractor;
 
+let rSlider;
+
 function setup() {
   createCanvas(400, 400);
 
@@ -13,10 +15,6 @@ function setup() {
   // create sliders
   rSlider = createSlider(0, 255, 100);
   rSlider.position(20, 20);
-  gSlider = createSlider(0, 255, 0);
-  gSlider.position(20, 50);
-  bSlider = createSlider(0, 255, 255);
-  bSlider.position(20, 80);
 
   emitter = new Emitter(width / 2, height / 2);
   repeller = new Repeller(width / 2, 300);
@@ -25,15 +23,11 @@ function setup() {
 
 
 function draw() {
-  background(220);
 
   const r = rSlider.value();
-  const g = gSlider.value();
-  const b = bSlider.value();
-  background(r, g, b);
-  text('red', rSlider.x * 2 + rSlider.width, 35);
-  text('green', gSlider.x * 2 + gSlider.width, 65);
-  text('blue', bSlider.x * 2 + bSlider.width, 95);
+
+  background(r);
+  text('alpha', rSlider.x * 2 + rSlider.width, 35);
  
   emitter.addParticle();
 
@@ -52,6 +46,4 @@ function draw() {
 
   repeller.show();
   attractor.show();
-
-  
 }
