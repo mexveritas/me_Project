bea26b3d461a04536ef4e06ff7787f0069f6d3ec
mexveritas <147470609+mexveritas@users.noreply.github.com>
2023-12-15 13:54:52 +0900

    Add files via upload

---
+++ repeller.js
@@ -0,0 +1,25 @@
+class Repeller {
+  constructor(x, y, power) {
+    this.position = createVector(x, y);
+
+    this.power = 150;
+    this.power = power;
+  }
+  
+  show() {
+    stroke(0);
+    strokeWeight(0);
+    fill(0, 120, 120);
+    circle(this.position.x, this.position.y, 25);
+  }
+
+  repel(particle) {
+ 
+    let force = p5.Vector.sub(this.position, particle.position);
+    let distance = force.mag();
+    distance = constrain(distance, 5, 50);
+    let strength = (-1 * this.power) / (distance * distance);
+    force.setMag(strength);
+    return force;
+  }
+}
